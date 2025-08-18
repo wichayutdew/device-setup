fa112955500ce8e1d20e6d97776882b81f33d227
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2025-08-19 00:33:14 +0700

    feat: add lsp keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,6 +37,7 @@ vim.pack.add({
 	{ src = 'https://github.com/nvim-telescope/telescope.nvim' },
 	{ src = 'https://github.com/tpope/vim-surround' },
 	{ src = 'https://github.com/github/copilot.vim' },
+	{ src = 'https://github.com/scalameta/nvim-metals' },
 })
 
 -- Language Server, Syntax Highlight, Code Completion ( Mason and LSP)
@@ -47,6 +48,13 @@ require "nvim-treesitter.configs".setup({
 })
 vim.lsp.enable({ "lua_ls", "kotlin_language_server" })
 vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = "Code formatting" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+vim.keymap.set("n", "gD", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "gR", vim.lsp.buf.references, { desc = "Find references" })
+vim.keymap.set("n", "rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(args)
 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
@@ -57,6 +65,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 })
 vim.cmd("set completeopt+=noselect")
+require "metals".initialize_or_attach({})
 
 
 -- sidebar
@@ -67,5 +76,5 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
 require("telescope").setup()
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
