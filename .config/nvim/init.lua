249a366fa3ded31a9f5d61c20e86720a87fabdbf
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2025-08-18 23:51:44 +0700

    feat: restart entire nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,71 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.winborder = "rounded"
+vim.opt.termguicolors = true
+
+-- Basic Keymap
+vim.keymap.set({ 'n', 'v', 'x' }, 'Y', '"+y')
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Windows Navigation
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>sh", ":split<CR>")
+
+-- Transparency and Colorscheme
+vim.cmd.colorscheme("unokai")
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+-- Package Manager
+vim.pack.add({
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+	{ src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+	{ src = 'https://github.com/nvim-lua/plenary.nvim' },
+	{ src = 'https://github.com/nvim-telescope/telescope.nvim' },
+	{ src = 'https://github.com/tpope/vim-surround' },
+	{ src = 'https://github.com/github/copilot.vim' },
+})
+
+-- Language Server, Syntax Highlight, Code Completion ( Mason and LSP)
+require "mason".setup()
+require "nvim-treesitter.configs".setup({
+	ensure_installed = { "lua", "kotlin" },
+	highlight = { enable = true }
+})
+vim.lsp.enable({ "lua_ls", "kotlin_language_server" })
+vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = "Code formatting" })
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+
+})
+vim.cmd("set completeopt+=noselect")
+
+
+-- sidebar
+require("nvim-tree").setup()
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+
+-- FuzzyFinder
+require("telescope").setup()
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
